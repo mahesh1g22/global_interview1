100ed235de7cd5f6fcbbf549cd6b19d73acf33df
mahesh1g22 <mahesh.1g22@gmail.com>
2022-10-13 00:09:52 +0100

    Added docstring comments for better understanding of the functions

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ from datetime import datetime
 from aws_storage import AWSStorage
 import pipeline
 
+'''
+This method prints the message on how to use and quit the program
+'''
 def printusageandquit(message: str):
     print(message)
     quit()
+'''
+Checks if the user passed the date or not
+'''
 def validateinput():
     if len(sys.argv) < 2:
         printusageandquit('Please pass the day as argument in YYYY-MM-DD format to process the data')
@@ -32,11 +38,13 @@ formateddate = date.strftime('%Y/%m/%d')
 data = storage_client.read_data(bucket=bucket, file_path=f"{formateddate}/{inputdate}.csv")
 
 status = data.get("ResponseMetadata", {}).get("HTTPStatusCode")
-
+'''
+Checks if the file requested is present in S3. If not prints the error message and quit the program
+'''
 if status == 200:
     result = pipeline.transform(data.get("Body"))
     csv_buffer = StringIO()
     result.to_csv(csv_buffer, index=False)
     storage_client.write_data(bucket=bucket,file_path=f"results/daily_agg_{date.strftime('%Y%m%d')}_Ven.csv", body=csv_buffer.getvalue())
 else:
-    print(f"Unsuccessful S3 get_object response. Status - {status}")
+    print(f"Unable to find the data for given date - {inputdate}")
--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 
+'''
+This method removes the duplicates by using IMPRESSION_ID, IMPRESSION_DATETIME and aggregate the data by CAMPAIGN_ID and hours.
+Out of the transform method holds the CAMPAIGN_ID, DAY_HOURS and COUNT
+'''
 def transform(csv_data):
     dict_data = pd.read_csv(csv_data).drop_duplicates(subset=['IMPRESSION_ID', 'IMPRESSION_DATETIME'])
     dict_data['DAY_HOUR'] = dict_data['IMPRESSION_DATETIME'].str[11:13]
